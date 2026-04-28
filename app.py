5f932277ba46ab1556776a929049952aa3509fdc
OliverOKeeffe <125363675+OliverOKeeffe@users.noreply.github.com>
2026-04-28 11:37:13 +0100

    Render wasnt working so reverted back, have to try and find another way to deploy

--- app.py
+++ app.py
@@ -17,8 +17,4 @@ app.layout = html.Div(
 )
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8050)),
-        debug=False,
-    )
+    app.run(debug=True)
